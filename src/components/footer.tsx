d714d379177dd58673fc2276e3a02e34e1e675ea
Yevgen Yakovliev <yyakovliev02@gmail.com>
2022-10-05 20:44:44 +0300

    feat: footer/navbar components and layout

---
+++ src/components/footer.tsx
@@ -0,0 +1,49 @@
+import Image from 'next/image';
+import React from 'react';
+
+export const Footer: React.FC = () => {
+  return (
+    <div className="footer container-full">
+      <div className="sub-footer">
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-6 footer-logo"><Image src="/images/global-mini/global-mini-logo-white.png" alt='Logo White' width={330} height={72} />
+              <Image src="/images/Blanchard-Norway-Logo-White.png" alt='Logo' width={300} height={167} className="visible-xs center-block" />
+              <div className="copyright copy">A global licensee of The Ken Blanchard Companies&reg;</div>
+            </div>
+            <div className="col-sm-3 address">
+              <div className="footer-title">BLANCHARD NORWAY</div>
+              Laberget 44<br />
+              N-4020 Stavanger<br />
+
+              <p><Image src="/images/top-nav-icon-phone.png" alt='Phone' width={12} height={12} /> 23 65 39 39</p>
+              <p className="visible-xs"><Image src="/images/top-nav-icon-email.png" width={16} height={12} alt='Email' /> <a href="mailto:info@blanchard.no">info@blanchard.no</a></p>
+            </div>
+            <div className="col-sm-3 social visible-lg">
+              <ul>
+                <li><a href="https://www.facebook.com/BlanchardNorway/" target="_blank" rel="noreferrer"><Image src="/images/footer-icon-fb.png" width={31} height={29} alt='Facebook' /></a></li>
+                <li><a href="https://twitter.com/leaderchat/" target="_blank" rel="noreferrer"><Image src="/images/footer-icon-twitter.png" width={31} height={29} alt='Twitter' /></a></li>
+                <li><a href="https://www.linkedin.com/company/12169/" target="_blank" rel="noreferrer"><Image src="/images/footer-icon-linkedin.png" width={31} height={29} alt='LinkedIn' /></a></li>
+                <li><a href="https://www.youtube.com/user/KenBlanchardCos" target="_blank" rel="noreferrer"><Image src="/images/footer-icon-youtube.png" width={31} height={29} alt='YouTube' /></a></li>
+              </ul>
+            </div>
+          </div>
+          <div className="row copyright">
+            <div className="col-sm-6 copy"></div>
+            <div className="col-sm-3 email hidden-xs"><Image src="/images/top-nav-icon-email.png" width={16} height={12} alt='Email' /> <a
+                href="mailto:info@blanchard.no">info@blanchard.no</a></div>
+          </div>
+          <div className="row copyright bottom">
+            <div className="col-sm-9 copy">&copy; Copyright 2017 The Ken Blanchard Companies. All Rights Reserved.</div>
+            <div className="col-sm-3">
+              <p className="privacy-map text-right"><span className="privacy"><a
+                    href="privacy-policy.html">Personvernserklæring</a></span>
+                <span className="map"><a href="site-map.html">Sidekart</a></span>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
