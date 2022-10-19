7d0a4a8393016b1de1714507ee065e383ecf3191
Yevgen Yakovliev <yyakovliev02@gmail.com>
2022-10-20 00:23:07 +0300

    feat: added product change link to navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -112,7 +112,9 @@ export const Navbar: React.FC = () => {
                     </a>
                   </li>
                   <li>
-                    <a href="product-change.html">Endringsledelse</a>
+                    <Link href={`${basePath}/products/change`}>
+                      Endringsledelse
+                    </Link>
                   </li>
                   <li>
                     <a href="product-coaching.html">Grunnleggende coaching</a>
