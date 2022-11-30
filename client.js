b5f760a91259a2260f33bd125600bd1f7af7c66b
Yevgen Yakovliev <yyakovliev02@gmail.com>
2022-11-30 12:09:02 +0200

    feat: updated sanity creds

--- client.js
+++ client.js
@@ -2,7 +2,7 @@
 import sanityClient from '@sanity/client';
 
 export default sanityClient({
-  projectId: 'w1we57qh', // you can find this in sanity.json
+  projectId: '3jo2f6oi', // you can find this in sanity.json
   dataset: 'production', // or the name you chose in step 1
   useCdn: true, // `false` if you want to ensure fresh data
 });
